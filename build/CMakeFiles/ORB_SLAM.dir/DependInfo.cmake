a4ceff56308687fc2e56153931af3dc884e7c3d0
Zhijun Zhu <zhijunzhu@yeah.net>
2019-10-30 17:23:57 +0800

    modification to satisfy the ros melodic

---
+++ build/CMakeFiles/ORB_SLAM.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zhizhu/workspaces/ORB_SLAM/src/Converter.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/Converter.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/Frame.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/Frame.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/FramePublisher.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/FramePublisher.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/Initializer.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/Initializer.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/KeyFrame.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/KeyFrame.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/KeyFrameDatabase.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/KeyFrameDatabase.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/LocalMapping.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/LocalMapping.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/LoopClosing.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/LoopClosing.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/Map.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/Map.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/MapPoint.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/MapPoint.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/MapPublisher.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/MapPublisher.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/ORBextractor.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/ORBextractor.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/ORBmatcher.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/ORBmatcher.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/Optimizer.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/Optimizer.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/PnPsolver.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/PnPsolver.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/Sim3Solver.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/Sim3Solver.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/Tracking.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/Tracking.cc.o"
+  "/home/zhizhu/workspaces/ORB_SLAM/src/main.cc" "/home/zhizhu/workspaces/ORB_SLAM/build/CMakeFiles/ORB_SLAM.dir/src/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_CB_DISABLE_DEBUG"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"ORB_SLAM\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/ros/melodic/include"
+  "/usr/include/opencv"
+  "../"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
